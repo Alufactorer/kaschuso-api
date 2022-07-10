025e848deee63be15ab612eabf351826e6538448
Aluminium <82846311+Alufactorer@users.noreply.github.com>
2022-07-10 18:35:00 +0200

    added get major and get classes function

--- sodseus.js
+++ sodseus.js
@@ -1,46 +1,40 @@
-
+const {Gradeobject, ClassesObject} = require('../helper/objects')
 const puppeteer = require('puppeteer')
 const os = require("os")
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-const getMajMAPS = {
-  "Big":0,
-  "Bio":1,
-  "Che":2,
-  "Deu":3,
-  "EngW":4,
-  "Fra":5, 
-  "Geo":6,
-  "Ges":7,
-  "Inf":8,
-  "Mat":9,
-  "Mus":10,
-  "PAMS":11,
-  "Phy":12,
-  "Spo":13
+
+async function getMaj(username, password) {
+  let index = -2;
+  if(os.platform() === "linux"){
+    browser = await puppeteer.launch({ headless:true, executablePath: "/usr/bin/chromium-browser", args: ['--no-sandbox', "--disabled-setupid-sandbox",'--disable-dev-shm-usage'] })
+  }
+  if(os.platform() === "win32"){
+    browser = await puppeteer.launch()    
+  }
+
+
+  const page = await browser.newPage();
+  await page.goto('https://kaschuso.so.ch/login/sls/auth?RequestedPage=%2fksso');
+  await page.type("input[name=userid]", username)
+  await page.click("img[src='staticfiles/images/eye.gif']")
+  await page.type("input[name=password]", password)
+  await page.click(".btn.btn-primary")
+  await page.waitForSelector('i.fa-graduation-cap', {visible: true})
+
+  let tabs = await page.$$('td[class="mdl-data-table__cell--non-numeric"]')
+  for(i = 0; i < tabs.length; i++){
+    let value = await page.evaluate(el => el.textContent, tabs[i])
+    if(value == "Ausbildungsgang") index = i
+    if(i == index + 1){
+        browser.close()
+        return value
+    }
+  }
 }
 
-const getMaj = {
-  "Big":0,
-  "Bio":1,
-  "Che":2,
-  "Deu":3,
-  "EngW":4,
-  "Fra":5, 
-  "Geo":6,
-  "Ges":7,
-  "Inf":8,
-  "Mat":9,
-  "Mus":10,
-  "PAMS":11,
-  "Phy":12,
-  "Spo":13
-}
-
-async function login(username, password, maj){
-  let gradeobject = []
-  console.log(os.platform())
+async function getClasses(username, password){
+    let gradearrayobject = []
   const r = /\B\s+|\s+\B/g
 
   if(os.platform() === "linux"){
@@ -61,10 +55,46 @@ async function login(username, password, maj){
 
   await page.click("i.fa-graduation-cap")
 
-  await page.waitForSelector(`tr[class='0_${getMaj[maj]}_detailrow'] > td > table > tbody > tr`)
+  await page.waitForSelector('i.fa-graduation-cap', {visible: true})
 
-  let rg = [];
-  let grades = await page.$$(`tr[class='0_${getMaj[maj]}_detailrow'] > td > table > tbody > tr`)
+  let allclasses = await page.$$("tr > td > b")
+  allclasses.pop()
+  let classes = [];
+  for(i = 0; i < allclasses.length; i++){
+    let value = await page.evaluate(el => el.textContent, allclasses[i])
+    classes.push(value.split("-")[0].split(" ")[0])
+  }
+
+  browser.close();
+  return new ClassesObject(classes)
+}
+
+async function getGrades(username, password, maj){
+  let classeslist = await getClasses(username, password);
+  let gradearrayobject = []
+  const r = /\B\s+|\s+\B/g
+
+  if(os.platform() === "linux"){
+    browser = await puppeteer.launch({ headless:true, executablePath: "/usr/bin/chromium-browser", args: ['--no-sandbox', "--disabled-setupid-sandbox",'--disable-dev-shm-usage'] })
+  }
+  if(os.platform() === "win32"){
+    browser = await puppeteer.launch()    
+  }
+
+
+  const page = await browser.newPage();
+  await page.goto('https://kaschuso.so.ch/login/sls/auth?RequestedPage=%2fksso');
+  await page.type("input[name=userid]", username)
+  await page.click("img[src='staticfiles/images/eye.gif']")
+  await page.type("input[name=password]", password)
+  await page.click(".btn.btn-primary")
+  await page.waitForSelector('i.fa-graduation-cap', {visible: true})
+
+  await page.click("i.fa-graduation-cap")
+
+  await page.waitForSelector(`tr[class='0_${classeslist[maj]}_detailrow'] > td > table > tbody > tr`)
+
+  let grades = await page.$$(`tr[class='0_${classeslist[maj]}_detailrow'] > td > table > tbody > tr`)
   grades.pop()
   grades.shift()
 
@@ -74,20 +104,20 @@ async function login(username, password, maj){
 
     let value = await page.evaluate(el => el.textContent, grade)
 
-    value.split("\n").map(t => t.replaceAll(" ", "")).map(n => {
-
-      if(n != ""){
-        ret.push(n)
-      }
+    value = value.split("\n").map(info => {
+        let stack = info.trim()
+        if(stack != ""){return stack}
+    }).filter(el => {
+        return (
+            el !== undefined
+        )
     })
-    ret = ret.map(s => s = (s.replace(/([A-Z])/g, ' $1').trim()))
-      if(ret.length == 5){
-        gradeobject.push({date:ret[0], info:ret[1], grade:ret[2], points:ret[3].split("sz")[0] + "s " + ret[3].split("Details")[1].split(":").join(" : "), weight:ret[4]})
-      }
-      if(ret.length == 4){
-        gradeobject.push({date:ret[0], info:ret[1], grade:ret[2], points:"no information on points", weight:ret[3]})
-      }
-
+    if(value.length == 4){
+        value.splice(3, 0, "no information on points available")
+    }
+    
+    gradeobject = new Gradeobject(value)
+    gradearrayobject.push(gradeobject)
   }
 
   
@@ -99,8 +129,11 @@ async function login(username, password, maj){
 
   await browser.close();
 
-  return gradeobject;
+  return gradearrayobject;
 }
+
+getGrades("alim.weber", "Venawa34puwa&", "BiG").then(console.log)
+
 //works but needs performance 
 
-module.exports = {login}
+module.exports = {getGrades, getClasses, getMaj}
